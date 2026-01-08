166f8d99b4169350e68cb010ace128954d964435
iu <doudou239@gmail.com>
2026-01-08 19:20:12 +0100

    Add refresh token session logic with new endpoint and service logic adjustments

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import * as cookie from 'cookie';
 import type { Request } from 'express';
@@ -18,11 +24,35 @@ export class AuthController {
     const cookies = cookie.parse(rawCookieHeader);
     const refreshToken = cookies['refreshToken'];
     // TODO if token is not found???
-    if (!refreshToken) return false;
+    if (!refreshToken) {
+      console.log('refresh Token is not found');
+      return false;
+    }
     console.log('refresh Token:', refreshToken);
     const result = await this.refreshTokenService.check(refreshToken);
     console.log('result:', result);
     return result;
+  }
+
+  @Get('refresh-tokens')
+  async refreshTokens(@Req() req: Request) {
+    try {
+      console.log('try to refresh session');
+      const rawCookieHeader = req.headers.cookie ?? '';
+      const cookies = cookie.parse(rawCookieHeader);
+      const refreshToken = cookies['refreshToken'];
+      if (!refreshToken) {
+        console.log('refresh Token is not found');
+        // TODO logout???
+        return false;
+      }
+      console.log('refresh Token:', refreshToken);
+      const result = await this.authService.refreshSession(refreshToken);
+      return result;
+    } catch (e) {
+      console.log('error:', e);
+      throw new HttpException((e as Error).message, HttpStatus.BAD_REQUEST);
+    }
   }
 
   @Get('revoke-refresh-token')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { RefreshTokenService } from '../refreshToken/refresh-token.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly refreshTokenService: RefreshTokenService) {
+  constructor(
+    private readonly refreshTokenService: RefreshTokenService,
+    private readonly usersService: UsersService,
+  ) {}
 
+  async refreshSession(refreshToken: string) {
+    /* 1) Check refresh token and get user ID
+     * 2) Find user by ID
+     * 3) Revoke old token
+     * 3) Generate new tokens (access + refresh)
+     * 4) Save refresh token to the DB */
+    console.log('try to refresh session');
+    console.log('refresh token from client: ', Boolean(refreshToken));
+
+    if (!refreshToken) {
+      console.log('Error: ', 'refreshToken is required');
+      throw new UnauthorizedException('refreshToken is required');
+    }
+
+    /* Check refresh token */
+    const check = await this.refreshTokenService.check(refreshToken);
+    if (!check) {
+      console.log('Error: ', 'Refresh token is invalid');
+      throw new UnauthorizedException('Refresh token is invalid');
+    }
+    console.log('Refresh token is valid');
+
+    // TODO activate
+    /* Revoking old token */
+    // await this.refreshTokenService.revoke(refreshToken);
+    // console.log('refreshToken revoked');
+
+    // TODO make universal and use also in login
+    /* Generate a new pair of tokens */
+    const tokens = this.refreshTokenService.generateTokens({
+      email: check.user.email,
+      nickname: check.user.nickname,
+    });
+    console.log('new refresh token: ', tokens.refreshToken);
+    console.log('new access token: ', tokens.accessToken);
+    /* Save a refresh token to the DB */
+    await this.refreshTokenService.save(check.user.id, tokens.refreshToken);
+    console.log('refresh token saved');
+
+    return tokens;
   }
 }
