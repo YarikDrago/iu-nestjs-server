6eb7357cfc90f295abd16decc758757093b89aa2
iu <doudou239@gmail.com>
2026-01-19 13:52:50 +0100

    Integrate `AuthModule` into `TournamentsModule` to enable authentication functionality.

--- src/tournaments/tournaments.module.ts
+++ src/tournaments/tournaments.module.ts
@@ -4,9 +4,14 @@ import { FootballModule } from '../football/football.module';
 import { TournamentsController } from './tournaments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tournaments } from './entities/tournament.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tournaments]), FootballModule],
+  imports: [
+    TypeOrmModule.forFeature([Tournaments]),
+    FootballModule,
+    AuthModule,
+  ],
   providers: [TournamentsService],
   exports: [TournamentsService],
   controllers: [TournamentsController],
